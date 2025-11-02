860dd8bf1f878928fc5b6d9f08919d07c749c4f2
Dean Kopping <dkopping9@gmail.com>
2025-11-02 20:51:23 +0200

    fix: wezterm padding

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -9,10 +9,9 @@ config.window_padding = {
 	bottom = 0,
 }
 config.use_fancy_tab_bar = true
-config.show_new_tab_button_in_tab_bar = false
 config.window_frame = {
 	font = wezterm.font({ family = "Roboto", weight = "DemiBold" }),
-	font_size = 12.0,
+	font_size = 14.0,
 }
 config.colors = {
 	tab_bar = {
@@ -34,9 +33,6 @@ config.keys = {
 }
 
 wezterm.on("format-tab-title", function(tab)
-	local process = tab.active_pane.foreground_process_name
-	local is_nvim = process and process:find("nvim")
-
 	-- Get just the last part of the path or process name
 	local title = tab.active_pane.title
 	title = title:match("([^/]+)$") or title
@@ -51,12 +47,6 @@ wezterm.on("format-tab-title", function(tab)
 	if tab.is_active then
 		return {
 			{ Background = { Color = "#1F1F28" } },
-			{ Foreground = { Color = "#ffffff" } },
-			{ Text = title },
-		}
-	elseif is_nvim then
-		return {
-			{ Background = { Color = "#b45309" } },
 			{ Foreground = { Color = "#ffffff" } },
 			{ Text = title },
 		}
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -68,3 +68,6 @@ cm() {
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+
+# To customize prompt, run `p10k configure` or edit ~/dotfiles/zsh/.p10k.zsh.
+[[ ! -f ~/dotfiles/zsh/.p10k.zsh ]] || source ~/dotfiles/zsh/.p10k.zsh
